f1fd57d5660b1158a2097dbf2018dc2324ae1db3
C.Vengels <6281096+cvengels@users.noreply.github.com>
2023-02-13 15:16:34 +0100

    Basic pickup test

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -7,9 +8,17 @@ public class PlayerController : MonoBehaviour
 
     public NavMeshAgent agent;
 
+    [SerializeField] private bool hasObjectInHand;
+    [SerializeField] private GameObject objectInHand;
+
+    private void Awake()
+    {
+        hasObjectInHand = false;
+        objectInHand.SetActive(false);
+    }
+    
     void Update()
     {
-
         if (Input.GetMouseButtonDown(0))
         {
             Ray ray = cam.ScreenPointToRay(Input.mousePosition);
@@ -20,6 +29,24 @@ public class PlayerController : MonoBehaviour
                 agent.SetDestination(hit.point);
             }
         }
-        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Resource"))
+        {
+            if (!hasObjectInHand)
+            {
+                hasObjectInHand = true;
+                objectInHand.SetActive(true);
+                objectInHand.GetComponent<MeshRenderer>().material =
+                    collision.collider.GetComponent<MeshRenderer>().material;
+            }
+        }
+        if (collision.collider.CompareTag("Resource Sink") && hasObjectInHand)
+        {
+            hasObjectInHand = false;
+            objectInHand.SetActive(false);
+        }
     }
 }
